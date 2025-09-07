15ce97745514efb92a36a2b233e968c738b54704
andrew0samir <andrewsamir317@gmail.com>
2025-09-07 21:35:07 +0300

    uploading images to supabase

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
 
@@ -15,11 +15,14 @@ export async function getCabins() {
 
 
 export async function createCabin(newCabin) {
+    const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll('/', '');
+    const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`
 
-    const { data, error } = await supabase
-        .from('cabins')
+    // 1. create the cabin 
+
+    const { data, error } = await supabase.from('cabins')
         .insert([
-            newCabin
+            { ...newCabin, image: imagePath }
         ]);
 
     if (error) {
@@ -27,14 +30,24 @@ export async function createCabin(newCabin) {
         throw new Error("Cabin could not be created")
     }
 
+    // 2. upload the image 
+    const { error: storageError } = await supabase.storage.from('cabin-images').upload(imageName, newCabin.image)
+
+    // 3. Delete the cabin if there was an error uploading the image 
+    if (storageError) {
+        await supabase.from('cabins')
+            .delete()
+            .eq('id', data.id)
+        console.error(storageError)
+        throw new Error("Cabin image could not be uploaded and cabin was not created")
+    }
     return data;
 
 }
 
 export async function deleteCabin(id) {
 
-    const { data, error } = await supabase
-        .from('cabins')
+    const { data, error } = await supabase.from('cabins')
         .delete()
         .eq('id', id)
     if (error) {
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://jauwotzcoqpsnnceqbbk.supabase.co'
+export const supabaseUrl = 'https://jauwotzcoqpsnnceqbbk.supabase.co'
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImphdXdvdHpjb3Fwc25uY2VxYmJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0MDk5MTYsImV4cCI6MjA3MTk4NTkxNn0.UooSMd-9t-duifHCkeTgRdASr9-y4_bLVr5_vqWYfVc"
 const supabase = createClient(supabaseUrl, supabaseKey)
 
