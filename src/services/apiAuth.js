ac76450e85b3cf79e8ba53de28410fcae5a44562
andrew0samir <andrewsamir317@gmail.com>
2025-10-19 21:42:24 +0300

    authorization , protecting routes

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,8 +1,8 @@
-import Supabase from "./supabase";
+import supabase from "./supabase";
 
 export async function login({ email, password }) {
 
-    const { data, error } = await Supabase.auth.signInWithPassword({
+    const { data, error } = await supabase.auth.signInWithPassword({
         email,
         password
     });
@@ -10,5 +10,17 @@ export async function login({ email, password }) {
     if (error) throw new Error(error.message);
 
     return data;
+
+}
+
+export async function getCurrentUser() {
+    const { data: session } = await supabase.auth.getSession();
+    if (!session.session) return null;
+
+    const { data, error } = await supabase.auth.getUser()
+
+    if (error) throw new Error(error.message);
+
+    return data?.user;
 
 }
