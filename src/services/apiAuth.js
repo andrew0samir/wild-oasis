05a1e8e828a73c08ec122b93a20a711ad3032ef9
andrew0samir <andrewsamir317@gmail.com>
2025-10-27 20:45:37 +0300

    updating user data and password

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function signup({ fullName, email, password }) {
     const { data, error } = await supabase.auth.signUp({
@@ -44,5 +44,36 @@ export async function logout() {
     const { error } = await supabase.auth.signOut();
 
     if (error) throw new Error(error.message);
+
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+    // 1. update the password OR full name 
+    let updateData;
+    if (password) updateData = { password };
+    if (fullName) updateData = { data: { fullName } }
+    const { data, error } = await supabase.auth.updateUser(updateData)
+
+    if (!avatar) return data;
+    if (error) throw new Error(error.message);
+
+
+    // 2. upload the avatar image 
+    const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+    const { error: storageError } = await supabase.storage.from('avatars').upload(fileName, avatar);
+
+    if (storageError) throw new Error(storageError.message);
+
+    // 3. update the avatar in the user
+    const { data: updatedUser, error: error2 } = await supabase.auth.updateUser({
+        data: {
+            avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+        }
+    })
+
+    if (error2) throw new Error(error2.message);
+
+    return updatedUser;
 
 }
