caa728436660f5bd76381ac60c7bba2d5bec7435
andrew0samir <andrewsamir317@gmail.com>
2025-11-08 23:23:07 +0200

    displaying a line chart with the recharts library

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import { useRecentStays } from "./useRecentStays";
 import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -37,6 +38,7 @@ function DashboardLayout() {
       />
       <div>statistics</div>
       <div>todays activity</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
